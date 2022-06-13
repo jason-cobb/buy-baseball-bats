b06bad70af496dda8bf57f7afbfb701674a17232
jason <jasoncobbatc@gmail.com>
2022-06-13 16:25:23 -0400

    Updated baseball bat model

--- ShopBaseballBats/ShopBaseballBats/Models/BaseballBats.cs
+++ ShopBaseballBats/ShopBaseballBats/Models/BaseballBats.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+//using System.Linq;
+//using System.Web;
 
 
 namespace ShopBaseballBats.Models
 {
     public class BaseballBats
-    {
-        public int Id { get; set; }
-        public int BatId { get; set; }
-        public BatModel[] BatModels { get; set; }
-        public string DescriptionHtml { get; set; }
-        public string BatBrand { get; set; }
-        public bool Favorite { get; set; }
+    {   
+               //BatBrand as a class or interface for LS, Easton, Rawlings etc.
+            public string BatBrand { get; set; }
+            public int Id { get; set; }
+            public int BatId { get; set; }
+            public BatModel[] BatModels { get; set; }
+            public string DescriptionHtml { get; set; }
+
+            public bool Favorite { get; set; }
+
+            public string DisplayText
+            {
+                get { return BatModels + DescriptionHtml; }
+            }
+            //series-title-issuenumber.jpg
+            //batmodel-year.jpg
+            public string CoverImageFileName
+            {
+                get
+                {
+                return "file.jpg"; //BatModels.Replace(" ", "-").ToLower() + "-" + BatModel.Length+ ".jpg";
+                }
+            }
+        
+        
     }
 }
