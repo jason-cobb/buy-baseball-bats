90026b52fab803d3b923444bef12cdd542d8cf15
jason <jasoncobbatc@gmail.com>
2022-06-13 15:46:44 -0400

    Added BaseballBats and BatModel models

---
+++ ShopBaseballBats/ShopBaseballBats/Models/BaseballBats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace ShopBaseballBats.Models
+{
+    public class BaseballBats
+    {
+        public int Id { get; set; }
+        public int BatId { get; set; }
+        public BatModel[] BatModels { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string BatBrand { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
---
+++ ShopBaseballBats/ShopBaseballBats/Models/BatModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopBaseballBats.Models
+{
+    public class BatModel
+    {   
+        public int Length { get; set; }
+        public int Weight { get; set; }
+        public double Price { get; set; }
+    }
+}
